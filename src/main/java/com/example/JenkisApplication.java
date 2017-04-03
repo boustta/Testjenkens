65b294d4b07ac7bb845fef5139ee0c78f9c583d7
Wafa <wafa@toto.com>
2017-04-03 16:21:47 +0200

    init project

---
+++ src/main/java/com/example/JenkisApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkisApplication.class, args);
+	}
+}
